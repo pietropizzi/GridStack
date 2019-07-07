2c63543cde9f9b7ce46f36ba1494bad0e203d821
Peter Minarik <peter@peterminarik.com>
2019-07-07 16:10:46 +0200

    Limit to iOS 13 and up

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GridStack",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
