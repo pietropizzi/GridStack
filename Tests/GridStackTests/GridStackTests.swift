e380852169d020bb9ec21d7a61fdc6e0a7d9735a
Peter Minarik <peter@peterminarik.com>
2019-07-08 22:21:41 +0200

    Add Calculator tests

--- Tests/GridStackTests/GridStackTests.swift
+++ Tests/GridStackTests/GridStackTests.swift
@@ -1,14 +1,78 @@
 import XCTest
 @testable import GridStack
 
-final class GridStackTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+final class GridCalculatorTests: XCTestCase {
+    
+    private var subject: GridCalculator!
+    
+    override func setUp() {
+        super.setUp()
+        subject = GridCalculator()
     }
-
+    
+    override func tearDown() {
+        subject = nil
+        super.tearDown()
+    }
+    
+    func test_threeCellsFitExactlyWithoutSpacing() {
+        let (columnWidth, columnCount) = subject.calculate(
+            availableWidth: 300,
+            minimumCellWidth: 100,
+            cellSpacing: 0
+        )
+        
+        XCTAssertEqual(columnWidth, 100)
+        XCTAssertEqual(columnCount, 3)
+    }
+    
+    func test_threeCellsFitExactlyWithSpacing() {
+        let (columnWidth, columnCount) = subject.calculate(
+            availableWidth: 340,
+            minimumCellWidth: 100,
+            cellSpacing: 10
+        )
+        XCTAssertEqual(columnWidth, 100)
+        XCTAssertEqual(columnCount, 3)
+    }
+    
+    func test_threeCellsJustDontFit() {
+        let (columnWidth, columnCount) = subject.calculate(
+            availableWidth: 339,
+            minimumCellWidth: 100,
+            cellSpacing: 10
+        )
+        XCTAssertEqual(columnWidth, 154.5)
+        XCTAssertEqual(columnCount, 2)
+    }
+    
+    func test_minimumCellWidthIsWiderThanAvailableSpace_givesOneColumn() {
+        let (columnWidth, columnCount) = subject.calculate(
+            availableWidth: 300,
+            minimumCellWidth: 310,
+            cellSpacing: 0
+        )
+        
+        XCTAssertEqual(columnCount, 1)
+        XCTAssertEqual(columnWidth, 300)
+    }
+    
+    func test_minimumCellWidthWithSpacingIsWiderThanAvailableSpace_givesOneColumn() {
+        let (columnWidth, columnCount) = subject.calculate(
+            availableWidth: 300,
+            minimumCellWidth: 290,
+            cellSpacing: 10
+        )
+        
+        XCTAssertEqual(columnCount, 1)
+        XCTAssertEqual(columnWidth, 280)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("test_cellsFitExactlyWithoutSpacing", test_threeCellsFitExactlyWithoutSpacing),
+        ("test_cellsFitExactlyWithSpacing", test_threeCellsFitExactlyWithSpacing),
+        ("test_threeCellsJustDontFit", test_threeCellsJustDontFit),
+        ("test_minimumCellWidthIsWiderThanAvailableSpace_givesOneColumn", test_minimumCellWidthIsWiderThanAvailableSpace_givesOneColumn),
+        ("test_minimumCellWidthWithSpacingIsWiderThanAvailableSpace_givesOneColumn", test_minimumCellWidthWithSpacingIsWiderThanAvailableSpace_givesOneColumn)
     ]
 }
--- Tests/GridStackTests/XCTestManifests.swift
+++ Tests/GridStackTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(GridStackTests.allTests),
+        testCase(GridCalculatorTests.allTests),
     ]
 }
 #endif
