fdad9672d2dae600c2d8f4b87b5cf090cfdf49cf
Peter Minarik <peter@peterminarik.com>
2019-12-09 09:53:18 +0100

    Add macOS Example app

---
+++ Example Apps/macOS/GridStackExampleMac/ContentView.swift
@@ -0,0 +1,21 @@
+import GridStack
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        GridStack(minCellWidth: 300, spacing: 2, numItems: 15) { index, cellWidth in
+            Text("\(index)")
+                .foregroundColor(.white)
+                .frame(width: cellWidth, height: cellWidth * 0.66)
+                .background(Color.blue)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
