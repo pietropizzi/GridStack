ceba07ddb28e36848d33677bfb62fbfba0804f0b
Daniel Witurna <daniel.witurna@gmail.com>
2022-02-21 13:36:02 +0100

    Prevent stride being called with non-positive Int

--- Sources/GridStack/Array+chunked.swift
+++ Sources/GridStack/Array+chunked.swift
@@ -1,6 +1,10 @@
 extension Array {
     func chunked(into size: Int) -> [[Element]] {
-        stride(from: 0, to: count, by: size).map {
+        guard size > 0 else {
+            return [self]
+        }
+        
+        return stride(from: 0, to: count, by: size).map {
             Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
